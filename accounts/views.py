9dbf6b7d1ffa4df5f26a77766a77d5f64a8ebd7d
atharva hiwase <atharva.hiwase07@gmail.com>
2021-01-17 18:18:40 +0530

    changes

--- accounts/views.py
+++ accounts/views.py
@@ -28,7 +28,6 @@ def home(request):
     pending = orders.filter(status='Pending').count()
     context = {'orders': orders, 'customers': customers, 'total_orders':total_orders,
     'delivered':delivered, 'pending':pending}
-
     return render(request, 'accounts/dashboard.html', context)
 
 def products(request):

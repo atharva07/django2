201cbdbb9067b1caab44eb8a3726c7cd0ca7501a
atharva hiwase <atharva.hiwase07@gmail.com>
2021-01-21 22:12:41 +0530

    major changes

--- accounts/views.py
+++ accounts/views.py
@@ -5,6 +5,7 @@ from .models import *
 from .forms import OrderForm, CreateUserFrom
 from .filters import OrderFilter
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 # Create your views here.
 
@@ -22,8 +23,24 @@ def registerPage(request):
     return render(request, 'accounts/register.html', context)
 
 def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'Username or password is incorrect')
+
     context = {}
     return render(request, 'accounts/login.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
 
 def home(request):
     orders = Order.objects.all()

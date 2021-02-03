6b5527931c1b82c6e53f6daf266e459d143ccf5d
atharva hiwase <atharva.hiwase07@gmail.com>
2021-02-03 18:12:42 +0530

    changes

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,8 +1,7 @@
 from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
-from django import forms
 from django.contrib.auth.models import User
-from .models import Order
+from .models import *
 
 class CustomerForm(ModelForm):
     class Meta:
